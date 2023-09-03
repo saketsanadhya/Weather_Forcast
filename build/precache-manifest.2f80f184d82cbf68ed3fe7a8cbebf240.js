8e2e0ffaf63ea3b5f403de466989ea7c7da23fd3
saketsanadhya <saketsanadhya2003@gmail.com>
2023-09-04 03:01:35 +0530

    Weather Api

---
+++ build/precache-manifest.2f80f184d82cbf68ed3fe7a8cbebf240.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af612690359cc5e42b55d83b5b3c75c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e323da34a570d3f0c208",
+    "url": "/static/css/main.ed2c4c53.chunk.css"
+  },
+  {
+    "revision": "001b67d05a335e326bff",
+    "url": "/static/js/2.6a7fe4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a7fe4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e323da34a570d3f0c208",
+    "url": "/static/js/main.c3cb4dd2.chunk.js"
+  },
+  {
+    "revision": "30259e7a30d4e8b4ab16",
+    "url": "/static/js/runtime-main.a9559881.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "75e0f8ca422777deab777d3abe0a90e7",
+    "url": "/static/media/background.75e0f8ca.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/static/media/city.e1698332.jpg"
+  }
+]);
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "1613fa2ff41e8ced3dffd5b4065caf15",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
